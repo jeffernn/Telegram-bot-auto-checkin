3498e18dae0ade28b8ab1a5dbfec374773b7f200
jeffernn <624670457@qq.com>
2025-07-02 14:55:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from telethon import TelegramClient, events
 import asyncio
 
 # 替换为你自己的 API 信息
-api_id = 24191107
-api_hash = 'f08f30015647d9d1af0ac9941d4dfbff'
-CHANNEL_ID = '@GetfreeCloud_Bot'
+api_id = 你的api_id
+api_hash = '你的api_hash'
+CHANNEL_ID = '@你要发送的机器人用户名'
 
 async def main():
     async with TelegramClient('chat_name', api_id, api_hash) as client:
-        # 发送 '/checkin'
+        # 发送 '/checkin' 可以修改为你需要的指令
         await client.send_message(CHANNEL_ID, '/checkin')
 
         # 定义一个 future 用于等待消息
